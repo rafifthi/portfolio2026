9d0a7e4ad1b0d8d73fcd5ee89d24f11d05fad5c0
Fip <rafifthii@gmail.com>
2026-07-12 22:49:58 +0700

    Update CV references to the Q3 2026 PDF

--- src/components/apps/CV.tsx
+++ src/components/apps/CV.tsx
@@ -11,20 +11,20 @@ export default function CV() {
           <Icon name="Printer" size={14} />
         </button>
         <a
-          href="/ATS PM 2026 - Rafif Fathi.pdf"
+          href="/ATS PM Q3 2026 - Rafif Fathi.pdf"
           download
           className="p-1.5 rounded hover:bg-gray-100 text-gray-500"
         >
           <Icon name="Download" size={14} />
         </a>
         <div className="flex-1" />
-        <span className="text-xs text-gray-400">ATS PM 2026 - Rafif Fathi.pdf</span>
+        <span className="text-xs text-gray-400">ATS PM Q3 2026 - Rafif Fathi.pdf</span>
       </div>
 
       {/* CV Document */}
       <div className="flex-1 overflow-hidden">
         <iframe
-          src="/ATS PM 2026 - Rafif Fathi.pdf"
+          src="/ATS PM Q3 2026 - Rafif Fathi.pdf"
           className="w-full h-full border-none"
           title="CV - Rafif Fathi Misbah"
         />
